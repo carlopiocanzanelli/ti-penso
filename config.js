67e782541e64c4fbdafd7345390c78a6b3bcf009
carlopiocanzanelli <carlopio.canzanelli@gmail.com>
2026-09-19 17:56:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// Compila questi tre valori (te li faccio trovare nella guida).
-// Sono tutti valori "pubblici": possono stare nel sito senza problemi.
 window.CONFIG = {
-  SUPABASE_URL: "https://gyhezfinivsaetydkmmo.supabase.co" ,        // es. https://abcdxyz.supabase.co
-  SUPABASE_ANON_KEY: "sb_publishable_w755pLXbZE18-T8VLK0UnQ_PpNLp-Cr",   // "anon" / "publishable"
+  SUPABASE_URL: "https://gyhezfinivsaetydkmmo.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_w755pLXbZE18-T8VLK0UnQ_PpNLp-Cr",
   VAPID_PUBLIC_KEY: "BE5ogFWaGOKiMK6U_XBvvMjkN6b9IsPSfehU940Pv15xcuE6RfDIu7uteA2kP4zx5GbiTlzTE4njxQZEdDTIyK0"
 };
